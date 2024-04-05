2f1bef3d2a489f4bdd911b7cc7ff3684c7ecfc5e
Dean Araujo <deantarik4@gmail.com>
2024-04-05 15:37:42 -0300

    userById

--- src/main/java/com/deanaraujo/workshopmongo/services/UserService.java
+++ src/main/java/com/deanaraujo/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.deanaraujo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.deanaraujo.workshopmongo.domain.User;
 import com.deanaraujo.workshopmongo.repository.UserRepository;
+import com.deanaraujo.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,7 +16,13 @@ public class UserService {
 	@Autowired //Autowired (inj. de dependência automática) faz com que o objeto seja instanciado
 	private UserRepository repo;
 	
+	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
