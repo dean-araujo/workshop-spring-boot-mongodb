b5934379887e7da3de8a631596dffd0eade18e64
Dean Araujo <deantarik4@gmail.com>
2024-04-08 15:45:47 -0300

    Post findById

---
+++ src/main/java/com/deanaraujo/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.deanaraujo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.deanaraujo.workshopmongo.domain.Post;
+import com.deanaraujo.workshopmongo.repository.PostRepository;
+import com.deanaraujo.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired //Autowired (inj. de dependência automática) faz com que o objeto seja instanciado
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
