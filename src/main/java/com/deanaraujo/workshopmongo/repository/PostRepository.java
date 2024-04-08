8aad843b9669b4163eac0663f63751d6a82cabd0
Dean Araujo <deantarik4@gmail.com>
2024-04-08 16:31:27 -0300

    Search with @Query

--- src/main/java/com/deanaraujo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/deanaraujo/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.deanaraujo.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.deanaraujo.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.deanaraujo.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
